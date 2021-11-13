3c01d00295dedc6a1bd30e79eeec5fe4504ddd38
C8338Soner <sonerpeker84@gmail.com>
2021-11-13 13:16:37 +0300

    hashs password for token

--- user_api/views.py
+++ user_api/views.py
@@ -2,15 +2,18 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import RegistrationSerializer
 from rest_framework.authtoken.models import Token
-
+from django.contrib.auth.hashers import make_password
 # Create your views here.
-
 @api_view(['POST'])
-
 def registration_view(request):
- if request.method == 'POST':
-  serializer = RegistrationSerializer(data = request.data)
-  data = {}
-  if serializer.is_valid():
-   account = serializer.save()
-   
+    if request.method == 'POST':
+        serializer = RegistrationSerializer(data=request.data)
+        data = {}
+        if serializer.is_valid():
+         password = serializer.validated_data.get('password')
+         serializer.validated_data['password'] = make_password(password)         
+         serializer.save()        
+         data = serializer.data
+        else:
+            data = serializer.errors
+        return Response(data)
